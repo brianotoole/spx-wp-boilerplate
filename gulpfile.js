f628ff8a635d2c80ad37cdf8ee4e822233d7d627
Brian O'Toole <brianotoole@Brians-MacBook-Pro.local>
2017-04-25 15:17:10 -0400

    configured gulp / initial commit

---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+// Gulp config file
+// Styles
+var styleSRC            = './assets/scss/style.scss'; // Path to main .scss file
+var styleDestination    = './'; // Path to place the compiled CSS file (theme root)
+// JS
+var jsVendorSRC         = './assets/js/vendors/*.js'; // Path to JS vendors folder
+var jsVendorDestination = './assets/js/'; // Path to place the compiled JS vendors file
+var jsVendorFile        = 'vendors'; // Compiled JS vendors file name
+var jsCustomSRC         = './assets/js/custom/*.js'; // Path to JS custom scripts folder
+var jsCustomDestination = './assets/js/'; // Path to place the compiled JS custom scripts file
+var jsCustomFile        = 'custom'; // Compiled JS custom file name
+
+// Paths
+var styleWatchFiles     = './assets/css/**/*.scss'; // Path to all *.scss files inside css folder and inside them
+var vendorJSWatchFiles  = './assets/js/vendors/*.js'; // Path to all vendors JS files
+var customJSWatchFiles  = './assets/js/custom/*.js'; // Path to all custom JS files
+
+var gulp         = require('gulp'); // Gulp of-course
+
+// Plugins: CSS
+var sass         = require('gulp-sass'); // Gulp pluign for Sass compilation
+var autoprefixer = require('gulp-autoprefixer'); // Autoprefixing magic
+var minifycss    = require('gulp-uglifycss'); // Minifies CSS files
+
+// Plugins: JS
+var concat       = require('gulp-concat'); // Concatenates JS files
+var uglify       = require('gulp-uglify'); // Minifies JS files
+
+// Plugins: Utilities
+var rename       = require('gulp-rename'); // Renames files E.g. style.css -> style.min.css
+var sourcemaps   = require('gulp-sourcemaps'); // sourcemaps
+var notify       = require('gulp-notify'); // Sends message notification to you
+
+// Tasks
+gulp.task('styles', function () {
+ 	gulp.src( styleSRC )
+		.pipe( sourcemaps.init() )
+		.pipe( sass( {
+			errLogToConsole: true,
+			outputStyle: 'compact',
+			//outputStyle: 'compressed',
+			// outputStyle: 'nested',
+			// outputStyle: 'expanded',
+			precision: 10
+		} ) )
+		.pipe( sourcemaps.write( { includeContent: false } ) )
+		.pipe( sourcemaps.init( { loadMaps: true } ) )
+		.pipe( autoprefixer(
+			'last 2 version',
+			'> 1%',
+			'safari 5',
+			'ie 8',
+			'ie 9',
+			'opera 12.1',
+			'ios 6',
+			'android 4' ) )
+
+		.pipe( sourcemaps.write ( styleDestination ) )
+		.pipe( gulp.dest( styleDestination ) )
+
+
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( minifycss( {
+			maxLineLen: 10
+		}))
+		.pipe( gulp.dest( styleDestination ) )
+		.pipe( notify( { message: 'TASK: "styles" Completed!', onLast: true } ) )
+});
+
+
+/**
+ * Task: vendorJS
+ *
+ * Concatenate and uglify vendor JS scripts.
+ *
+ * This task does the following:
+ * 		1. Gets the source folder for JS vendor files
+ * 		2. Concatenates all the files and generates vendors.js
+ * 		3. Renames the JS file with suffix .min.js
+ * 		4. Uglifes/Minifies the JS file and generates vendors.min.js
+ */
+gulp.task( 'vendorsJs', function() {
+	gulp.src( jsVendorSRC )
+		.pipe( concat( jsVendorFile + '.js' ) )
+		.pipe( gulp.dest( jsVendorDestination ) )
+		.pipe( rename( {
+			basename: jsVendorFile,
+			suffix: '.min'
+		}))
+		.pipe( uglify() )
+		.pipe( gulp.dest( jsVendorDestination ) )
+		.pipe( notify( { message: 'TASK: "vendorsJs" Completed!', onLast: true } ) );
+});
+
+
+gulp.task( 'customJS', function() {
+ 	gulp.src( jsCustomSRC )
+		.pipe( concat( jsCustomFile + '.js' ) )
+		.pipe( gulp.dest( jsCustomDestination ) )
+		.pipe( rename( {
+			basename: jsCustomFile,
+			suffix: '.min'
+		}))
+		.pipe( uglify() )
+		.pipe( gulp.dest( jsCustomDestination ) )
+		.pipe( notify( { message: 'TASK: "customJs" Completed!', onLast: true } ) );
+});
+
+// Watch tasks
+ gulp.task( 'default', [ 'styles', 'vendorsJs', 'customJS' ], function () {
+ 	gulp.watch( styleWatchFiles, [ 'styles' ] );
+ 	gulp.watch( vendorJSWatchFiles, [ 'vendorsJs' ] );
+ 	gulp.watch( customJSWatchFiles, [ 'customJS' ] );
+ });
